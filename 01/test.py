e1b47fd9053f7f2c6ec0dcec46a6730ba26bae8e
Timofei <timofei.psheno@gmail.com>
2022-09-13 22:43:55 +0300

    Исправление test.py + coverage_report

--- 01/test.py
+++ 01/test.py
@@ -1,14 +1,15 @@
 import unittest
-import sys, os
+import sys
+import os
 import cross
 
 
 class TestCross(unittest.TestCase):
     def setUp(self) -> None:
         self.obj = cross.Game()
-        sys.stdout = open(os.devnull, 'w')
+        sys.stdout = open(os.devnull, 'w', encoding='utf8')
     def test_input(self):
-        self.base_table = [['1', 'X', '3',
+        base_table = [['1', 'X', '3',
                        '4', '5', '6',
                        '7', '8', '9'], # occup
                       ['1', '2', '3',
@@ -20,16 +21,17 @@ class TestCross(unittest.TestCase):
                        ['1', '2', '3',
                        '4', '5', 'O',
                        '7', 'X', '9']] # OK
-        self.base_input = ['2', '10', 'X', '1']
-        self.base_ans = [False, False, False, True]
-        for test in range(len(self.base_ans)):
-            self.obj.board = self.base_table[test]
-            self.assertEqual(self.obj.take_input(self.base_input[test]), self.base_ans[test])
+        base_input = ['2', '10', 'X', '1']
+        base_ans = [False, False, False, True]
+        buf_len = len(base_ans)
+        for test in range(buf_len):
+            self.obj.board = base_table[test]
+            self.assertEqual(self.obj.take_input(base_input[test]), base_ans[test])
 
     def test_draw(self):
-        self.base_table = [['1', 'X', '3',
+        base_table = [['1', 'X', '3',
                        '4', '5', '6',
-                       '7', '8', '9'], 
+                       '7', '8', '9'],
                       ['1', '2', '3',
                        '4', '5', '6',
                        '7', '8', '9'],
@@ -39,12 +41,12 @@ class TestCross(unittest.TestCase):
                        ['1', '2', '3',
                        '4', '5', 'O',
                        '7', 'X', '9']]
-        for i in self.base_table:
+        for i in base_table:
             self.obj.board = i
             self.assertIsNone(self.obj.draw())
 
     def test_win(self):
-        self.base_table = [['1', 'X', '3',
+        base_table = [['1', 'X', '3',
                        '4', '5', '6',
                        '7', '8', '9'], #Nobody wins
                       ['X', '2', 'O',
@@ -52,21 +54,23 @@ class TestCross(unittest.TestCase):
                        'O', '8', 'X'], #X wins
                        ['1', 'X', '3',
                        'O', 'O', 'O',
-                       '7', 'X', 'X']] #O wins                      
-        self.base_ans = [False, 'X', 'O']
-        for test in range(len(self.base_ans)):
-            self.obj.board = self.base_table[test]
-            self.assertEqual(self.obj.check_win(), self.base_ans[test])
+                       '7', 'X', 'X']] #O wins
+        base_ans = [False, 'X', 'O']
+        buf_len = len(base_ans)
+        for test in range(buf_len):
+            self.obj.board = base_table[test]
+            self.assertEqual(self.obj.check_win(), base_ans[test])
 
     def test_allgame(self):
-        self.games = [['5', '1', '6', '4', '7', '3', '9', '8', '2'],
+        games = [['5', '1', '6', '4', '7', '3', '9', '8', '2'],
                       ['5', '6', '3', '8', '7'],]
-        self.result = [0, 1]
-        for i in range(len(self.games)):
+        result = [0, 1]
+        buf_len = len(games)
+        for i in range(buf_len):
             self.obj.board = list(range(1,10))
-            for x in self.games[i][:-1]:
-                self.obj.game_start(auto=True, pos=x)
-            self.assertEqual(self.obj.game_start(auto=True, pos=self.games[i][-1]), self.result[i])
+            for x_data in games[i][:-1]:
+                self.obj.game_start(auto=True, pos=x_data)
+            self.assertEqual(self.obj.game_start(auto=True, pos=games[i][-1]), result[i])
 
     def tearDown(self) -> None:
         sys.stdout.close()

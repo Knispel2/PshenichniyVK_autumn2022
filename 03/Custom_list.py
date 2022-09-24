0abc499c8025da6cfe166a914eda039ed5fcf741
Timofei <timofei.psheno@gmail.com>
2022-09-25 00:51:24 +0300

    General functions

--- 03/Custom_list.py
+++ 03/Custom_list.py
@@ -1,33 +1,35 @@
 ﻿from functools import reduce
+import copy
 
 class Custom_list(list):
-    def __sub__(self, obj):
-        buf_self = copy(self)
-        buf_obj = copy(obj)
+    def __sub__(self, obj, reverse=False):
+        buf_self = copy.copy(self)
+        buf_obj = copy.copy(obj)
+        if reverse: buf_self, buf_obj = buf_obj, buf_self
         buf_min = min(buf_self, buf_obj, key=len)
-        buf_len = min(len(buf_self), len(buf_obj))
-        Custom_plot.refill(buf_min, buf_len)
-        result = map(lambda x, y : return x-y, buf_self, buf_obj)
+        buf_len = max(len(buf_self), len(buf_obj))
+        Custom_list.refill(buf_min, buf_len)
+        result = map(lambda x, y : x-y, buf_self, buf_obj)
         return Custom_list(result)
 
     def __rsub__(self, obj):
-        return self.__sub__(obj)
+        return self.__sub__(obj, True)
 
     def __add__(self, obj):
-        buf_self = copy(self)
-        buf_obj = copy(obj)
+        buf_self = copy.copy(self)
+        buf_obj = copy.copy(obj)
         buf_min = min(buf_self, buf_obj, key=len)
-        buf_len = min(len(buf_self), len(buf_obj))
-        Custom_plot.refill(buf_min, buf_len)
-        result = map(lambda x, y : return x+y, buf_self, buf_obj)
+        buf_len = max(len(buf_self), len(buf_obj))
+        Custom_list.refill(buf_min, buf_len)
+        result = map(lambda x, y : x+y, buf_self, buf_obj)
         return Custom_list(result)
 
     def __radd__(self, obj):
-        return self.__sub__(obj)
+        return self.__add__(obj)
 
     @staticmethod
     def list_to_int(obj):
-        return reduce(lambda x, y: return x+y, obj, initializer=0)
+        return reduce(lambda x, y: x+y, obj, 0)
 
     @staticmethod
     def refill(obj, data_len):
@@ -53,4 +55,5 @@ class Custom_list(list):
         return Custom_list.list_to_int(self) >= Custom_list.list_to_int(other)
     
     def __str__(self):
-        return f"{list(map(lambda x : print(x)), self)}, {Custom_list.list_to_int(self)}"
+        return f"{[x for x in self]}, {Custom_list.list_to_int(self)}"
+
--- 03/test.py
+++ 03/test.py
@@ -3,21 +3,30 @@ import sys
 import os
 import Custom_list
 
-
-class TestJSON(unittest.TestCase):
+class Test_Clist(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.Custom_list = Custom_list.Custom_list
 
     def test_sub(self):
-        self.assertEqual(Custom_list([5, 1, 3, 7]) - Custom_list([1, 2, 7]), Custom_list([4, -1, -4, 7]))
+        self.assertEqual(self.Custom_list([5, 1, 3, 7]) - self.Custom_list([1, 2, 7]), self.Custom_list([4, -1, -4, 7]))
+        self.assertIsInstance(self.Custom_list([5, 1, 3, 7]) - self.Custom_list([1, 2, 7]), type(self.Custom_list([4, -1, -4, 7])))
 
     def test_add(self):
-        self.assertEqual(Custom_list([5, 1, 3, 7]) - Custom_list([1, 2, 7]), Custom_list([6, 3, 10, 7]))
+        self.assertEqual(self.Custom_list([5, 1, 3, 7]) + self.Custom_list([1, 2, 7]), self.Custom_list([6, 3, 10, 7]))
+        self.assertIsInstance(self.Custom_list([5, 1, 3, 7]) + self.Custom_list([1, 2, 7]), type(self.Custom_list([6, 3, 10, 7])))
 
-    def test_not_is(self):
 
     def test_sub_list(self):
+        self.assertEqual(self.Custom_list([5, 1, 3, 7]) - [1, 2, 7], self.Custom_list([4, -1, -4, 7]))
+        self.assertEqual([5, 1, 3, 7] - self.Custom_list([1, 2, 7]), self.Custom_list([4, -1, -4, 7]))
+        self.assertIsInstance(self.Custom_list([5, 1, 3, 7]) - [1, 2, 7], type(self.Custom_list([4, -1, -4, 7])))
+        self.assertIsInstance([5, 1, 3, 7] - self.Custom_list([1, 2, 7]), type(self.Custom_list([4, -1, -4, 7])))
 
     def test_add_list(self):
+        self.assertEqual([5, 1, 3, 7] + self.Custom_list([1, 2, 7]), self.Custom_list([6, 3, 10, 7]))
+        self.assertEqual(self.Custom_list([5, 1, 3, 7]) + [1, 2, 7], self.Custom_list([6, 3, 10, 7]))
+        self.assertIsInstance([5, 1, 3, 7] + self.Custom_list([1, 2, 7]), type(self.Custom_list([6, 3, 10, 7])))
+        self.assertIsInstance(self.Custom_list([5, 1, 3, 7]) + [1, 2, 7], type(self.Custom_list([6, 3, 10, 7])))
 
-    def test_not_is(self):
+    def test_str(self):
+        self.assertEqual(str(self.Custom_list([1,1,1])), "[1, 1, 1], 3")

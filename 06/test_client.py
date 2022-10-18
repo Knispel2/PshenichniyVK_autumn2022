1e82f14cd0d7a7228bb31eb728dcebde9dd9fe27
Timofei <timofei.psheno@gmail.com>
2022-10-18 14:09:08 +0300

    Фиксация перед переходом на раздельное тестирование

--- 06/test_client.py
+++ 06/test_client.py
@@ -3,17 +3,17 @@ from mock import patch
 import client
 import server
 import threading
+import os
 from io import StringIO
 
 
 class TestClient(unittest.TestCase):
     def setUp(self) -> None:
         self.client = client.client_on
-        self.server_test = threading.Thread(target=server.server_on, args=(['server.py', '-w', '10', '-k', '7']))
 
     @patch('sys.stdout', new_callable=StringIO)
-    def main_test(self, mock_stdout):
-        self.server_test.start()
+    def test_main(self, mock_stdout):
+        os.system('python server.py -w 10 -k 7')
         self.client(['client.py', '10', 'urls.txt'])
         buf = mock_stdout.getvalue().split('/n')
         self.assertEqual(len(buf), 101)
--- 06/test_server.py
+++ 06/test_server.py
@@ -1,25 +1,25 @@
 import unittest
+from mock import patch
 import client
 import server
 import threading
+import os
+from io import StringIO
 
+test_data = '''CLient started...
+404 error: otvet.mail.ru/question/230876505
+https://otvet.mail.ru/question/230876538{"": 7892, "=": 175, "<span": 130, "<a": 91, "</span>\n": 72, "\u0438": 70, "<div": 68}
+404 error: fubiewbfiwufbuiewb
+404 error: 1321421321'''
 
 class TestServer(unittest.TestCase):
     def setUp(self) -> None:
-        self.cache = lru.MyLRU(2)
+        self.client = client.client_on
 
-    def test_one(self):
-        self.cache.set("k1", "val1")
-        self.cache.set("k2", "val2")
-        self.assertEqual(self.cache.get("k3"), None)
-        self.assertEqual(self.cache.get("k2"), "val2")
-        self.assertEqual(self.cache.get("k1"), "val1")
-
-    def test_two(self):
-        self.cache = lru.MyLRU(2)
-        self.test_one()
-        self.cache.set("k3", "val3")
-        self.assertEqual(self.cache.get("k3"), "val3")
-        self.assertEqual(self.cache.get("k2"), None)
-        self.assertEqual(self.cache.get("k1"), "val1")
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_main(self, mock_stdout):        
+        self.server_test = threading.Thread(target=server.server_on, args=(['server.py', '-w', '10', '-k', '7']))
+        self.server_test.start()
+        self.client(['client.py', '10', 'urls_test.txt'])
+        self.assertEqual(mock_stdout.getvalue(), test_data)
 

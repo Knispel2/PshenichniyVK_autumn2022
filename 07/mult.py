0cd1a47e66ede65421d51c84c3dfa7863e06d842
Timofei <timofei.psheno@gmail.com>
2022-10-27 11:15:01 +0300

    Pylint + flake8

--- 07/mult.py
+++ 07/mult.py
@@ -1,10 +1,10 @@
 import ctypes
 
 
-def c_matmult(a, b):
+def c_matmult(a_arg, b_arg):
     libmatmult = ctypes.CDLL("./07/mult.so")
-    dima = len(a) * len(a)
-    dimb = len(b) * len(b)
+    dima = len(a_arg) * len(a_arg)
+    dimb = len(b_arg) * len(b_arg)
     array_a = ctypes.c_float * dima
     array_b = ctypes.c_float * dimb
     array_c = ctypes.c_float * dima
@@ -12,27 +12,32 @@ def c_matmult(a, b):
     sumb = array_b()
     sumc = array_c()
     inda = 0
-    for i in range(0, len(a)):
-        for j in range(0, len(a[i])):
-            suma[inda] = a[i][j]
+    indb = 0
+    for i in enumerate(a_arg):
+        for j in enumerate(a_arg[i]):
+            suma[inda] = a_arg[i][j]
             inda = inda + 1
-        indb = 0
-    for i in range(0, len(b)):
-        for j in range(0, len(b[i])):
-            sumb[indb] = b[i][j]
+    for i in enumerate(b_arg):
+        for j in enumerate(b_arg[i]):
+            sumb[indb] = b_arg[i][j]
             indb = indb + 1
-    libmatmult.multMatrixSqBad(ctypes.byref(suma), ctypes.byref(sumb), ctypes.byref(sumc), len(a));
-    res = [[0 for _ in range(len(a))] for _ in range(len(a))]
+    libmatmult.multMatrixSqBad(ctypes.byref(suma),
+                               ctypes.byref(sumb),
+                               ctypes.byref(sumc),
+                               len(a_arg))
+    res = [[0 for _ in enumerate(a_arg)]
+           for _ in enumerate(a_arg)]
     indc = 0
-    for i in range(0, len(sumc)):
-        res[indc][i % len(a)] = sumc[i]
-        if i % len(a) == len(a) - 1:
+    for i in enumerate(sumc):
+        res[indc][i % len(a_arg)] = sumc[i]
+        if i % len(a_arg) == len(a_arg) - 1:
             indc = indc + 1
     return res
 
 
-def matmult(a,b):
-    zip_b = zip(*b)
+def matmult(a_arg, b_arg):
+    zip_b = zip(*b_arg)
     zip_b = list(zip_b)
-    return [[sum(ele_a*ele_b for ele_a, ele_b in zip(row_a, col_b))
-             for col_b in zip_b] for row_a in a]
+    return [[sum(ele_a*ele_b for ele_a, ele_b
+                 in zip(row_a, col_b))
+             for col_b in zip_b] for row_a in a_arg]
--- 07/test.py
+++ 07/test.py
@@ -1,24 +1,30 @@
-import numpy
 import time
+import numpy
 import mult
 
 MAX = 50
 
+
 def main():
-    a = [numpy.random.randint(0, 100, (n, n)).tolist() for n in range(MAX) if n!=0]
-    b = [numpy.random.randint(0, 100, (n, n)).tolist() for n in range(MAX) if n!=0]
+    a_arg = [numpy.random.randint(0, 100, (n, n)).tolist()
+             for n in range(MAX) if n != 0]
+    b_arg = [numpy.random.randint(0, 100, (n, n)).tolist()
+             for n in range(MAX) if n != 0]
     print("==== python ====")
     start_ts = time.time()
-    res_py = [mult.matmult(x, y) for x, y in zip(a, b)]
+    res_py = [mult.matmult(x, y) for x, y in zip(a_arg, b_arg)]
     end_ts = time.time()
-    print(f"Time of execution of python implementation is {end_ts-start_ts} seconds")
+    print(f'''Time of execution of python implementation
+ is {end_ts-start_ts} seconds''')
     print("==== ctypes ====")
     start_ts = time.time()
-    res_c = [mult.c_matmult(x, y) for x, y in zip(a, b)]
+    res_c = [mult.c_matmult(x, y) for x, y in zip(a_arg, b_arg)]
     end_ts = time.time()
-    print(f"Time of execution of ctypes implementation is {end_ts-start_ts} seconds")
-    res_numpy = [numpy.matmul(x, y).tolist() for x, y in zip(a, b)]
+    print(f'''Time of execution of ctypes implementation
+ is {end_ts-start_ts} seconds''')
+    res_numpy = [numpy.matmul(x, y).tolist() for x, y in zip(a_arg, b_arg)]
     assert res_py == res_c == res_numpy
+
 
 if __name__ == "__main__":
     main()
